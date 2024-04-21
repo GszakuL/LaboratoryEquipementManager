d9ac38d2a76c9dd86e32b24a3bbdeb90fcecaa53
lgajewski <lukasz.gajewski96@wp.pl>
2024-04-21 22:41:56 +0200

    add DeviceController

---
+++ Web/Controllers/ApiController.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public abstract class ApiController : ControllerBase
+    {
+        private ISender _sender;
+        protected ISender Sender => _sender ??= HttpContext.RequestServices.GetService<ISender>();
+
+    }
+}
---
+++ Web/Controllers/DeviceController.cs
@@ -0,0 +1,21 @@
+﻿using Application.Devices.Commands;
+using Application.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/devices")]
+    public class DeviceController : ApiController
+    {
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> AddDevice(AddDeviceDto addDeviceDto, CancellationToken cancellationToken)
+        { 
+            var command = new CreateDeviceCommand(addDeviceDto);
+
+            var deviceIdentificationNumber = await Sender.Send(command, cancellationToken);
+            
+            return Ok(deviceIdentificationNumber);
+        }
+    }
+}
