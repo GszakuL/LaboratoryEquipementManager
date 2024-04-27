1fde0c6f565a8bbaf40ff249f69acb7a64bc2bb0
lgajewski <lukasz.gajewski96@wp.pl>
2024-04-27 23:23:55 +0200

    fix namespaces

---
+++ Infrastructure/Migrations/20240424234743_RemoveIndexesFromPhysicalMagnitudeTable.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveIndexesFromPhysicalMagnitudeTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_PhysicalMagnitudes_Name",
+                table: "PhysicalMagnitudes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PhysicalMagnitudes_Unit",
+                table: "PhysicalMagnitudes");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Unit",
+                table: "PhysicalMagnitudes",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "PhysicalMagnitudes",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Unit",
+                table: "PhysicalMagnitudes",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "PhysicalMagnitudes",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PhysicalMagnitudes_Name",
+                table: "PhysicalMagnitudes",
+                column: "Name",
+                unique: true,
+                filter: "[Name] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PhysicalMagnitudes_Unit",
+                table: "PhysicalMagnitudes",
+                column: "Unit",
+                unique: true);
+        }
+    }
+}
